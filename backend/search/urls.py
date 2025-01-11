c26ab4ec958d4b955621b3f32556a5a20f573cac
Luis Galvan <luismgc.001@hotmail.com>
2025-01-10 19:44:17 -0500

    Modificacion de README

--- backend/search/urls.py
+++ backend/search/urls.py
@@ -3,7 +3,6 @@ from .views import ScrapingSearchView, SearchAndETLView, ListaDeseosView, Articu
 
 urlpatterns = [
     path("search/", ScrapingSearchView.as_view(), name="search"),
-    path("search-etl/", SearchAndETLView.as_view(), name="search_etl"),
     path("lista-deseos/", ListaDeseosView.as_view(), name="lista_deseos"),
     path("lista-deseos/<int:pk>/", ArticuloDetalleView.as_view(), name="detalle_deseo"),
 ]
